63645d6eff536f8ee5987c60898f1ba55c30bb69
Majid Ali Khan <majidalykhan@gmail.com>
2023-02-08 20:18:25 +0500

    Types in arrays

--- 01-typescript/04-arrays.ts
+++ 01-typescript/04-arrays.ts
@@ -2,3 +2,20 @@ const fruits = ["apple", "mango", "cherry"];
 const dates = [new Date(), new Date()];
 
 const cars: string[][] = [["civic"], ["corolla"], ["swift"]];
+
+//Help with inference when extracting values
+const fruit = fruits[0];
+const myFruit = fruits.pop();
+
+//Prevent incompatible values
+// fruits.push(20);
+
+//Help with map
+fruits.map((frt: string): string => {
+  return frt;
+});
+
+//Flexible
+const importantDates: (Date | string)[] = [];
+importantDates.push(new Date());
+importantDates.push("2030-05-25");
