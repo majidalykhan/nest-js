24cb5709023a4c84cf6d5028f8ca6f376609f0ca
Majid Ali Khan <majidalykhan@gmail.com>
2023-02-08 15:16:02 +0500

    Fixing the Any type

--- 01-typescript/02-annotations.ts
+++ 01-typescript/02-annotations.ts
@@ -33,5 +33,5 @@ const logNumber: (i: number) => void = (i: number) => {
 
 //1: Function that returns the 'any' type
 const json = '{"x":10, "y":20}';
-const coordinates = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
